755cf7a2151c2dea974be957c80f10c90da482ac
Cauê Almeida <nncl@live.com>
2015-04-30 08:30:00 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -12,35 +12,38 @@ if (function_exists('register_sidebar'))
     ));
 }
 
-/*******************************
-Update the login image page
-********************************/
-function my_custom_login_logo() {
-echo '<style type="text/css">
-	body.login div#login h1 a {
 
-		background-image: url(/your-path/file.png);
-		width: 100px;
-		-webkit-background-size: 100px 74px;
-		background-size: 100px 74px;
+// Update the login image page
+	function my_custom_login_logo() {
+	echo '<style type="text/css">
+		body.login div#login h1 a {
+	
+			background-image: url(/your-path/file.png);
+			width: 100px;
+			-webkit-background-size: 100px 74px;
+			background-size: 100px 74px;
+		}
+	</style>';
 	}
-</style>';
-}
+// End login image
 
 add_action('login_head', 'my_custom_login_logo');
 
-//Link na tela de login para a página inicial 
-function my_login_logo_url() {
-    return get_bloginfo( 'url' );
-}
-add_filter( 'login_headerurl', 'my_login_logo_url' );
- 
-function my_login_logo_url_title() {
-    return 'Website Title Here';
-}
-add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+// A link to your homepage on Login page
+	function my_login_logo_url() {
+	    return get_bloginfo( 'url' );
+	}
+	add_filter( 'login_headerurl', 'my_login_logo_url' );
+	 
+	function my_login_logo_url_title() {
+	    return 'Website Title Here';
+	}
+	add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+// End link
 
-//Colocar função que reconhece Imagem Destacada nos posts
+//Featured images
+	// code...
+//End Featured images
 
  
 ?>
